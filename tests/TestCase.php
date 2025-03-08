930d669a18b6432dd02bb7b68906030026bccd02
Julien Lambé <julien@themosis.com>
2025-03-08 06:16:52 +0100

    Add tools current configuration files.

---
+++ tests/TestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vendor\Namespace\Tests;
+
+use PHPUnit\Framework\TestCase as FrameworkTestCase;
+
+// phpcs:disable:Universal.Classes.RequireFinalClass
+class TestCase extends FrameworkTestCase
+{
+}
