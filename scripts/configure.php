7fd7b2ebe08430a4c8172814a3ce06c4d3442fae
Julien Lambé <julien@themosis.com>
2025-03-12 21:53:12 +0100

    Start configure script implementation. Add path utilities and ability to read a json file.

--- scripts/configure.php
+++ scripts/configure.php
@@ -1,3 +1,23 @@
 <?php
 
-echo "Package configuration...\n";
+declare(strict_types=1);
+
+namespace Themosis;
+
+function rootPath(): string {
+    return dirname(__DIR__);
+}
+
+function path(?string $path = null): string {
+    return $path ? rootPath().DIRECTORY_SEPARATOR.trim($path,'\/') : rootPath();
+}
+
+function json(string $path): \stdClass {
+    return \json_decode(
+        json: \file_get_contents($path),
+        associative: false,
+        flags: JSON_THROW_ON_ERROR,
+    );
+}
+
+var_dump(path('composer.json'), json(path('composer.json')));
