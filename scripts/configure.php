c2351b6fcf6a7fa4c8fc8de1c4cf86e739a63b09
Julien Lambé <julien@themosis.com>
2025-03-18 13:26:36 +0100

    Experiment with stdin and stdout.

--- scripts/configure.php
+++ scripts/configure.php
@@ -4,15 +4,18 @@ declare(strict_types=1);
 
 namespace Themosis;
 
-function rootPath(): string {
-    return dirname(__DIR__);
+function rootPath(): string
+{
+    return \dirname(__DIR__);
 }
 
-function path(?string $path = null): string {
-    return $path ? rootPath().DIRECTORY_SEPARATOR.trim($path,'\/') : rootPath();
+function path(?string $path = null): string
+{
+    return $path ? rootPath() . DIRECTORY_SEPARATOR . trim($path, '\/') : rootPath();
 }
 
-function json(string $path): \stdClass {
+function json(string $path): \stdClass
+{
     return \json_decode(
         json: \file_get_contents($path),
         associative: false,
@@ -20,4 +23,18 @@ function json(string $path): \stdClass {
     );
 }
 
-var_dump(path('composer.json'), json(path('composer.json')));
+function prompt(string $message, ?string &$value): void
+{
+    fwrite(STDOUT, $message."\n");
+    $value = rtrim(fgets(STDIN));
+}
+
+function writeLine(string $message): void
+{
+    fwrite(STDOUT, $message."\n");
+}
+
+$name = null;
+prompt("What's your name?", $name);
+writeLine("Welcome, {$name}!");
+
